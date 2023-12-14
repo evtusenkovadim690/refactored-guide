0349a923acd50eff20fce99942da9ff3ef2496a2
Jefflayman <151014290+evtusenkovadim690@users.noreply.github.com>
2023-12-15 01:06:16 +0530

    Create refactored-guide.swift

---
+++ refactored-guide.swift
@@ -0,0 +1,8 @@
+enum StickerModification {
+    case changeMask(UIImage)
+    case changeBlendMode(ContentBlendMode)
+enum TextModification {
+    case changeSize(CGFloat)
+    case changeFontName(String)
+    case changeKern(CGFloat)
+    case changeLineSpacing(CGFloat)
